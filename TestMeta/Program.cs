a3ba0ca5f858926d93d94e1ffa3a56b1ffd99f6d
Leandro Consiglio <leandroconsiglio@hotmail.com>
2025-11-03 10:48:52 -0300

    initial commit

--- TestMeta/Program.cs
+++ TestMeta/Program.cs
@@ -8,9 +8,9 @@ namespace TestMeta
     {
         static void Main(string[] args)
         {
-            var accessToken = "EAALuih0ZCysEBP70kOrLesZA5H4D8a873K2WrHAsB3pK3aGIGb88x9olpDYZA9BnSslarzQ7X4y2m2LKNc97xNEUlIVgPXKxLrZCBAdSKHdyiVbZBaZAV7fcLRabutC6Mrac2sgWkChWFyZA3Y8MUBj6kJ6OYHKVonX1Gz2myDNKs6YvGOggjal7JjOPGyPadCpoXX8N8g6ZCGAeXwcB0DLobmikDZCrpkSz6kVYkU6NrPDZAf5rUCksIDAPbyBjKd0p43QFp2MZAdeYX7t";
-            var appSecret = "4737648220f588a85eb875c8d11f8995";
-            var addAccount = "249358639";
+            var accessToken = "";
+            var appSecret = "";
+            var addAccount = "";
 
             var context = new APIContext(accessToken, appSecret);
 
